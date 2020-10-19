0066e690f8b34934ba93a50c30a88c1cd374fd03
unknown <nehathakur764@gmail.com>
2020-10-19 18:45:58 +0530

    Welcome to Employee Wage Computation

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,18 +3,10 @@ package com.computation;
 public class EmployeeWageComputation {
 
     public static void main(String args[]){
+
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION");
 
-        //TO check employee is present or absent
-        int IS_FULL_TIME = 1;
-        double empStatus = Math.floor(Math.random() * 10) % 2;
-        System.out.println(empStatus); //random value stored in empStatus;
-
-        if (empStatus == IS_FULL_TIME)
-            System.out.println("Employee is Present");
-        else
-            System.out.println("Employee is Absent");
-
+       
     }
 
 }
