d59d38aeca3fe1fbf737346a8a16178b4ec81933
unknown <nehathakur764@gmail.com>
2020-10-20 11:41:41 +0530

    To find Employee Parttime Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -6,20 +6,25 @@ public class EmployeeWageComputation {
 
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION");
 
-        //Daily Wage Of Employee
-        int isPresent=1;
+        //Part Time Wage Of Employee
+        int isPartTime=1;
+        int isFullTime=2;
         int empRatePerHr=20;
         int EmpWage=0;
         int empHrs=0;
 
-        double randomCheck = Math.floor(Math.random() * 10) % 2;
-        if(isPresent == randomCheck)
+        double randomCheck = Math.floor(Math.random() * 10) % 3;
+        if(isPartTime == randomCheck)
+                empHrs=4;
+
+     	else if(isFullTime == randomCheck)
                 empHrs=8;
      	else
-                empHrs=0;
+     	        empHrs=0;
 
      	EmpWage=empRatePerHr*empHrs ;
         System.out.println("Emp Wage: " + EmpWage);
+
     }
 
 }
